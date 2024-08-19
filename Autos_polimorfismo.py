f592f8e5ad9fa172c613fa6cabbacba2403032d0
diegoMS553 <145505027+diegoMS553@users.noreply.github.com>
2024-08-19 10:37:31 -0600

    Add files via upload

--- Autos_polimorfismo.py
+++ Autos_polimorfismo.py
@@ -1,6 +1,28 @@
-.........
-Run run run
+# Clase base (superclase) 
+class Automovil: 
+    def __init__(self, marca, año): 
+        self.marca = marca 
+        self.año = año 
+    def hacer_sonido(self): 
+        return "Sonido genérico" 
 
+# Subclase que hereda de Animal 
+class Electrico(Automovil): 
+    def hacer_sonido(self): 
+        return "........." 
 
-** Process exited - Return Code: 0 **
-Press Enter to exit terminal
+# Otra subclase que hereda de Animal 
+class Combustion(Automovil): 
+    def hacer_sonido(self): 
+        return "Run run run" 
+
+# Función que demuestra polimorfismo 
+def imprimir_sonido(animal): 
+    print(animal.hacer_sonido()) 
+    
+# Ejemplo de uso 
+model_3 = Electrico("Tesla", 2020) 
+chevi = Combustion("Chevrolet", 2002) 
+
+imprimir_sonido(model_3)  # Imprime: .........
+imprimir_sonido(chevi)  # Imprime: Run run run
